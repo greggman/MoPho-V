8e86d9afded759efcdcde1a57a03a651cb0e84ae
Gregg Tavares <github@greggman.com>
2018-05-24 20:13:45 +0900

    generate source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 // This is only for the Web code
 module.exports = {
   entry: './src/js/pages/web/web.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'web.js',
     path: path.resolve(__dirname, 'out/js/pages/web'),
